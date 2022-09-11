a2be3d8dfc94b74c9eee19d51585c65b737530d8
Stanislav2022 <tsv5@ukr.net>
2022-09-11 19:44:41 +0300

    Update StatisticsList.jsx

--- src/components/Statistics/StatisticsList/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList/StatisticsList.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
 export default function StatisticsList({ items }) {
-    console.log(items);
-    const elements = items.map(({ id, label, percentage }) => 
+    const elements = items.map(({ id, label, percentage }) =>
         <li className="item" key={id}>
             <span className="label">{label}</span>
             <span className="percentage">{percentage}%</span>
         </li>
     )
+    console.log(elements);
   return (
       <ul className="stat-list">{ elements }</ul>
   )
