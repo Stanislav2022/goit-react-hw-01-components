0071211465708dcbf7877cf132734ccbd815e9d9
Stanislav2022 <tsv5@ukr.net>
2022-09-11 18:45:40 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics";
 import user from "./user/user.json"
-
+import data from "./data/data.json"
 
 export const App = () => {
   return (
       <div>
-        <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats}/>
+      <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
       </div>
   );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
-export default function Statistics() {
+export default function Statistics(stats) {
+    console.log(stats)
   return (
-    <div>Statistics</div>
+    <section className="statistics">
+        <h2 className="title">Upload stats</h2>
+
+        <ul className="stat-list">
+            <li className="item">
+                <span className="label">.docx</span>
+                <span className="percentage">4%</span>
+            </li>
+            <li className="item">
+                <span className="label">.mp3</span>
+                <span className="percentage">14%</span>
+            </li>
+            <li className="item">
+                <span className="label">.pdf</span>
+                <span className="percentage">41%</span>
+            </li>
+            <li className="item">
+                <span className="label">.mp4</span>
+                <span className="percentage">12%</span>
+            </li>
+        </ul>
+    </section>
   )
 }
+
+// function Color(){
+//      this.r = Math.floor(Math.random()*255);
+//      this.g = Math.floor(Math.random()*255);
+//      this.b = Math.floor(Math.random()*255);
+//      this.color = 'rgba('+ this.r +','+ this.g +','+ this.b +',0.8)';
+//   }
