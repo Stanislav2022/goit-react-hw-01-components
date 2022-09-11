6a8139e50001e08249aa18b08ddb7725d604e119
Stanislav2022 <tsv5@ukr.net>
2022-09-11 21:18:12 +0300

    6

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,19 +1,42 @@
-import StatisticsList from "./StatisticsList/StatisticsList"
-import css from './StatisticsList/StatisticsList.modules.css'
+import StatisticsList from "./StatisticsList/StatisticsList";
+import x from './StatisticsList/StatisticsList.module.css';
+import css from '../Profile/Profile.module.css';
 
 export default function Statistics({ title, stats }) {
   return (
-      <section className="statistics">
+      <section className={x.statistics}>
           {title && <h2 className={css.title}>Upload stats</h2>}
-          <StatisticsList items={ stats } />
+          <StatisticsList items={stats} />
+           <ul className={css.statList}>
+        {stats.map(stat => {
+          return (
+            <li
+              key={stat.id}
+              className={css.item}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{stat.label}</span>
+              <span className={css.percentage}>{stat.percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
 
     </section>
   )
 }
 
-// function Color(){
-//      this.r = Math.floor(Math.random()*255);
-//      this.g = Math.floor(Math.random()*255);
-//      this.b = Math.floor(Math.random()*255);
-//      this.color = 'rgba('+ this.r +','+ this.g +','+ this.b +',0.8)';
-//   }
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+// Statistics.propTypes = {
+//   title: PropTypes.string,
+//   stats: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.string.isRequired,
+//       label: PropTypes.string.isRequired,
+//       percentage: PropTypes.number.isRequired,
+//     })
+//   ),
+// };
--- src/components/Statistics/StatisticsList/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList/StatisticsList.jsx
@@ -1,8 +1,9 @@
-import css from './StatisticsList.modules.css'
+import React from 'react';
+import css from './StatisticsList.modules.css';
 
 export default function StatisticsList({ items }) {
     const elements = items.map(({ id, label, percentage }) =>
-        <li className={css.item} key={id}>
+        <li className={css.item} key={id} style={{ backgroundColor: getRandomHexColor() }}>
             <span className={css.label}>{label}</span>
             <span className={css.percentage}>{percentage}%</span>
         </li>
@@ -12,4 +13,8 @@ export default function StatisticsList({ items }) {
       <ul className={css.stat__list}>{ elements }</ul>
   )
 }
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
