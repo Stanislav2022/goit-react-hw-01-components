1f659321da2d068c405733ea2299bc142c4ca381
Stanislav2022 <tsv5@ukr.net>
2022-09-12 23:49:00 +0300

    5

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import TransactionHistoryTable from './TransactionHistoryTable';
+import css from './TransactionHistory.module.css'
 
-export default function TransactionHistory() {
-  return (
-    <div>TransactionHistory</div>
+
+export default function TransactionHistory({items}) {
+    const elements = items.map(({ id, type, amount, currency }) => <TransactionHistoryTable key={id} type={type} amount={amount} currency={currency} />)
+    return (
+      <table className={css.transaction__history}>
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Amount</th>
+                <th>Currency</th>
+            </tr>
+        </thead>
+        <tbody>{elements}</tbody>
+    </table>
   )
 }
